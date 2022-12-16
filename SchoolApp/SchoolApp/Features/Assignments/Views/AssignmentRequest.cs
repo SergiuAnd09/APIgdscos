4510bf76bdb0ea76618ae98c8c0a9a594990bc18
SergiuAnd09 <sergiuopreasao@gmail.com>
2022-12-16 20:21:08 +0200

    prim

---
+++ SchoolApp/SchoolApp/Features/Assignments/Views/AssignmentRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolApp.Features.Assignments.Views;
+
+public class AssignmentRequest
+{
+    [Required]public string Subject { get; set; }
+    
+    [Required]public string Description { get; set; }
+    
+    [Required]public DateTime Deadline { get; set; }
+    
+    
+}
