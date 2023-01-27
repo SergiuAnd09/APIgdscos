7bcad52ddf9e8260ee88cf3265e8cbefb9df4ff3
SergiuAnd09 <sergiuopreasao@gmail.com>
2023-01-27 17:43:16 +0200

    secondo

--- SchoolApp/SchoolApp/Program.cs
+++ SchoolApp/SchoolApp/Program.cs
@@ -1,6 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using SchoolApp.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//aici facem comanda de legare la modificarea appsetting.Development.json
+var connectionString = builder.Configuration.GetConnectionString("Default");
+
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddControllers();    
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
